ec5fa72e1e4b7c490d54acf16f2e15e837101dd7
MikhaMGL <lebedev.mg@gmail.com>
2023-11-17 18:30:06 +0100

    corrector married

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -9,14 +9,16 @@ let passportMarried = {
 
 let passportMarried2 = {...passportMarried,  married: true, address: {...passportMarried.address}} 
 passportMarried2.married = true
- 
+passportWithAddress2.name = "Ivan"
+passportWithAddress2.address.city = "Bobryisk"
+
 console.log(passportMarried); 
 console.log(passportMarried2);
 
 
 // let passportWithAddress2 = {...passportWithAddress, address: {...passportWithAddress.address}}
-// passportWithAddress2.name = "Ivan"
-// passportWithAddress2.address.city = "Bobryisk"
+// // passportWithAddress2.name = "Ivan"
+// // passportWithAddress2.address.city = "Bobryisk"
 
 // console.log(passportWithAddress.address.city);
 // console.log(passportWithAddress2.address.city);

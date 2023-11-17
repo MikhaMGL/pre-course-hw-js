1f849f89eea07ff16937695b3104261c9c853ea6
MikhaMGL <mikhaillebedev@MacBook-Pro-MikhailLebedev.local>
2023-11-17 15:27:24 +0100

    next corrector

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,10 +2,10 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-let passport2 = {...passport};
+let passport2 = {...passport}
 passport2.name  = "Ivan"
-passport2.surname  = "Ivanov"
+
 
 
 console.log(passport);
-console.log(passport2)
+console.log(passport2);

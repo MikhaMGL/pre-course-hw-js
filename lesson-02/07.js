bfbe26298e38c0a552c4876737af3979bd7da99a
MikhaMGL <lebedev.mg@gmail.com>
2023-11-17 18:24:29 +0100

    add married

---
+++ lesson-02/07.js
@@ -0,0 +1,22 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportMarried2 = {...passportMarried,  married: true, address: {...passportMarried.address}} 
+passportMarried2.married = true
+ 
+console.log(passportMarried); 
+console.log(passportMarried2);
+
+
+// let passportWithAddress2 = {...passportWithAddress, address: {...passportWithAddress.address}}
+// passportWithAddress2.name = "Ivan"
+// passportWithAddress2.address.city = "Bobryisk"
+
+// console.log(passportWithAddress.address.city);
+// console.log(passportWithAddress2.address.city);

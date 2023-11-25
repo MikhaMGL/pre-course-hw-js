2ae9bdcb35feed0cc5355d3ef1291600a8e112b4
MikhaMGL <lebedev.mg@gmail.com>
2023-11-26 00:31:41 +0100

    filterDeutcheFlag

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,2 +1,10 @@
 // codec
 
+let colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(color => color === "черный" || color === "красный" || color === "желтый").join("-") 
+}
+
+console.log(createColorString());
+
